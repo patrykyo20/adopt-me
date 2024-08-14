78f94a40631591b98716eef900a467b5ae67ec21
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-08-14 11:56:30 +0200

    feat: add headling event to Carousel Class component

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,6 +9,12 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index
+    })
+  }
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -17,8 +23,11 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((image, index) => (
+            // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
               key={image}
+              data-index={index}  
               src={image}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
