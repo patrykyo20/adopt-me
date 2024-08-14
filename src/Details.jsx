010f2877a4cd7aed6136fd21741a0c8933f75c0a
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-08-14 14:26:22 +0200

    feat: show the modal in details

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,8 +3,11 @@ import { useParams } from "react-router-dom";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -26,8 +29,19 @@ const Details = () => {
         <h1>{pet.name}</h1>
         <h2>
           {pet.animal} - {pet.breed} - {pet.city} - {pet.state}
-          <button>Adopt {pet.name}</button>
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
           <p>{pet.description}</p>
+          {showModal && (
+            <Modal>
+              <div>
+                <h2>Would you like to adpot {pet.name}?</h2>
+                <div className="buttons">
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          )}
         </h2>
       </div>
     </div>
